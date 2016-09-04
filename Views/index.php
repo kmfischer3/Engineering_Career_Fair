281bcd2fb97db930c72fc0e590c5bff8f42291d1
Kristina Fischer <kmfischer3@wisc.edu>
2016-09-04 16:22:27 -0500

    dynamically generating company list from json data

--- Views/index.php
+++ Views/index.php
@@ -9,11 +9,32 @@
 
 </script>
 
-<div class="content">
-
-
-
+<div id="company_list" class="content">
 </div>
+
+<script type="text/javascript">
+
+var content_div = document.getElementById("company_list");
+
+for (var i = 0; i < data.length; i++) {
+	console.log(i);
+
+	var id 		= data[i]['id'];
+	var name 	= data[i]['name'];
+
+	var div = document.createElement("div");
+	var media_body = document.createElement("div");
+	var h4 = document.createElement("h4");
+	h4.class = "media-heading";
+	h4.innerHTML = name;
+	
+	div.appendChild(media_body)
+	media_body.appendChild(h4);
+
+	content_div.appendChild(div);
+}
+
+</script>
 
 <?php include("../includes/page_bottom.php");?>
 
