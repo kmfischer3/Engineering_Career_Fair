fb77ef9e9a4857d3d83c065d7cb6cc7049afafa0
Kristina Fischer <kmfischer3@wisc.edu>
2016-09-07 16:34:54 -0500

    move data into its own file

--- includes/page_top.php
+++ includes/page_top.php
@@ -5,8 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="../includes/css/styles.css">
   <script src="../includes/js/history_manager.js" type="text/javascript"></script>
+  <script src="../Views/data.php" type="text/javascript"></script>
+  <script src="../includes/js/functions.js"></script>
   <title>ECS Job Fair 2016</title>
 </head>
 <body>
