04d5b961a7ff8f2f7458e0d4097f854fc8a7d115
Kristina Fischer <kmfischer3@wisc.edu>
2016-09-08 17:56:33 -0500

    replaced hard-coded degree-position tables with dynamically generated versions pulling from db

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -2,32 +2,126 @@ function load_company_profile(company_id) {
 
 	company = data[company_id];
 	
-	citizen = [];
-	citizen_mask = company.citizen_mask;
+	// display the company name in the jumbotron div
+	$("#company_profile_name").html('<h1>' + company.name + '</h1>');
 	
-	if ((citizen_mask & 4) == 4) {
-		citizen.push("US Citizen");
-	}
-	if ((citizen_mask & 2) == 2) {
-		citizen.push("US Permanent Resident");
-	}
-	if ((citizen_mask & 1) == 1) {
-		citizen.push("Visa Holder");
-	}
-
+	
+	// create day_company_booth list with buttons to trigger the map view with the corresponding booth highlighted
 	$("#company_profile_day_list").html('<h4>Booth Locations</h4>\
 					     <a href="#" class="list-group-item"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Monday, Sept. 19 <span class="glyphicon glyphicon-menu-right pull-right"></span></a>\
 					     <a href="#" class="list-group-item"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Wednesday, Sept. 21 <span class="glyphicon glyphicon-menu-right pull-right"></span></a>\
 					     <a href="#" class="list-group-item"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Wednesday, Sept. 28 <span class="glyphicon glyphicon-menu-right pull-right"></span></a>');
-		    /*
-		    <div>\
-		    	<ul>\
-		    		<li>name: ' + company.name + '</li>\
-		    		<li>website: ' + company.website + '</li>\
-		    		<li>description: ' + company.description + '</li>\
-		    		<li>citizen: ' + citizen.join(", "); + '</li>\
-		    	</ul>\
-		    </div>';*/
+	
+	
+	// construct degree-position table
+	// if the company's degree_mask_# has anything other than '0000' for a given major, add that major to the table
+	// then, use (degree_mask & position_mask) to place the checkmark in the appropriate column
+	var table_body = '<tbody>';
+	
+	
+	/* POPULATE DEGREE MASK 1 ROWS*/
+	for (var d_key in degree_mask_1_array) {
+	
+		if ( ( company.degree_mask_1 & degree_mask_1_array[d_key] ) != 0 ) {
+	
+			table_body += '<tr><th scope="row">' + d_key + '</th>';
+			
+			for (var p_key in position_mask_array) {
+				
+				if ( ( company.degree_mask_1 & degree_mask_1_array[d_key] & position_mask_array[p_key] ) != 0 ) {
+					table_body += '<td><span class="glyphicon glyphicon-ok"></span></td>';
+				} else {
+					table_body += '<td></td>';
+				}
+			
+			}
+
+			table_body += '</tr>';
+		}
+	}
+	
+	/* POPULATE DEGREE MASK 2 ROWS*/
+	for (var d_key in degree_mask_2_array) {
+	
+		if ( ( company.degree_mask_2 & degree_mask_2_array[d_key] ) != 0 ) {
+	
+			table_body += '<tr><th scope="row">' + d_key + '</th>';
+			
+			for (var p_key in position_mask_array) {
+				
+				if ( ( company.degree_mask_2 & degree_mask_2_array[d_key] & position_mask_array[p_key] ) != 0 ) {
+					table_body += '<td><span class="glyphicon glyphicon-ok"></span></td>';
+				} else {
+					table_body += '<td></td>';
+				}
+			
+			}
+
+			table_body += '</tr>';
+		}
+	}
+	
+	/* POPULATE DEGREE MASK 3 ROWS*/
+	for (var d_key in degree_mask_3_array) {
+	
+		if ( ( company.degree_mask_3 & degree_mask_3_array[d_key] ) != 0 ) {
+	
+			table_body += '<tr><th scope="row">' + d_key + '</th>';
+			
+			for (var p_key in position_mask_array) {
+				
+				if ( ( company.degree_mask_3 & degree_mask_3_array[d_key] & position_mask_array[p_key] ) != 0 ) {
+					table_body += '<td><span class="glyphicon glyphicon-ok"></span></td>';
+				} else {
+					table_body += '<td></td>';
+				}
+			
+			}
+
+			table_body += '</tr>';
+		}
+	}
+		
+	table_body += '</tbody>';	
+	
+	
+	// insert the table body into the rest of the table html
+	var table_data = '\
+			<h3>Postion</h3>\
+			<table class="table">\
+				<thead>\
+					<tr>\
+						<th></th>\
+						<th>Intern</th>\
+						<th>Co-op</th>\
+						<th>Entry Level</th>\
+						<th>Experienced</th>\
+					</tr>\
+				</thead>\
+				'+table_body+'\
+			</table>\
+	';
+	
+	// populate the table div with the table created above
+	$("#company_profile_degree_position_table").html(table_data);
+
+	// create a citizenship string based on the company's citizenship mask, then add it to the div
+
+	var citizen = [];
+	var citizen_mask = company.citizen_mask;
+	
+	if ((citizen_mask & US_mask) == US_mask) {
+		citizen.push("US Citizen");
+	}
+	if ((citizen_mask & PR_mask) == PR_mask) {
+		citizen.push("US Permanent Resident");
+	}
+	if ((citizen_mask & VH_mask) == VH_mask) {
+		citizen.push("Visa Holder");
+	}
+
+	$("#company_profile_citizenship").html('<h4>Citizenship requirements: <small> ' + citizen.join(", ") + '</small></h4>');
+
 
 	$("#company_list").addClass('hidden');
 	$("#company_profile").removeClass('hidden');
