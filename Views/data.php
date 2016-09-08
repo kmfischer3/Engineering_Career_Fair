869d47867cf744ab5828eea865edbd5f1acb3d3f
Kristina Fischer <kmfischer3@wisc.edu>
2016-09-08 18:27:25 -0500

    deleting more unused code

--- Views/data.php
+++
@@ -1,6 +0,0 @@
-<?php
- header('Content-type: text/javascript');
- require_once("../includes/db_connection.php");
- require_once("../includes/functions.php");
-?>
-var data = <?php echo get_company_db_json(); ?>;
