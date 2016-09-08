af991d66035416aa519c283b1d7d33cbcb84d3c7
Austin Hartzheim <inbox@austinhartzheim.me>
2016-09-08 16:56:55 -0500

    Modify data.php to merge company and booth data into the same object.

--- Views/data.php
+++ Views/data.php
@@ -1,7 +1,6 @@
 <?php
  header('Content-type: text/javascript');
- require_once("../includes/db_connection.php"); 
+ require_once("../includes/db_connection.php");
  require_once("../includes/functions.php");
 ?>
 var data = <?php echo get_company_db_json(); ?>;
-var map_data = <?php echo get_day_company_booth_db_json(); ?>;
