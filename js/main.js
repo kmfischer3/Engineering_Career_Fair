7c119740c1edab456e47d9343383a3b14d1e5319
Austin Hartzheim <inbox@austinhartzheim.me>
2016-09-11 20:34:38 +0000

    Moved start code to js/main.js.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Once the DOM is ready, display the company list
+$(document).ready(function() {
+    load_companies();
+});
+
+// After the document load event is fired, load the SVG map in the background
+window.addEventListener("load", function() {
+    $("#map_svg").load("/static/map.svg");
+});
