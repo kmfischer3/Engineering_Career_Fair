18cb1c6280653d1b6c3579e20069ef6690433509
Austin Hartzheim <inbox@austinhartzheim.me>
2016-09-12 18:10:54 +0000

    Added map.highlightCompanies, which is able to highlight companies on the map, based on a selected day.
    This is a step in the direction of allowing maps to display multiple
    days worth of data.

--- js/map.js
+++ js/map.js
@@ -3,17 +3,35 @@ var map = {
     MAX_TABLE_NUMBER: 100,  // TODO: update
 
     resetTables: function() {
-	for (i = map.MIN_TABLE_NUMBER; i < map.MAX_TABLE_NUMBER; i++) {
-	    var table = document.getElementById("table" + i.toString());
-	    if (table == null)
-		continue;
+	      for (i = map.MIN_TABLE_NUMBER; i < map.MAX_TABLE_NUMBER; i++) {
+	          var table = document.getElementById("table" + i.toString());
+	          if (table == null)
+		            continue;
 
-	    table.classList.remove("highlight");
-	}
+	          table.classList.remove("highlight");
+	      }
     },
 
     highlightTable: function(table_id) {
-	var table = document.getElementById("table" + table_id.toString());
-	table.classList.add("highlight");
+	      var table = document.getElementById("table" + table_id.toString());
+	      table.classList.add("highlight");
+    },
+
+    /*
+     * options = {
+     *   company_ids: [1,2,3],
+     *   day: 0
+     * }
+     */
+    highlightCompanies: function(options) {
+
+        map.resetTables();
+
+        for (var company_id in options.company_ids) {
+            var company = data[company_id];
+
+            if (company.tables[options.day] != null)
+                map.highlightTable(company.tables[options.day]);
+        }
     }
 };
