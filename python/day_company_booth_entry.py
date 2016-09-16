7fca7f961675d2f4f638dbad9f7fa2899d9c4dd6
Kristina Fischer <kmfischer3@wisc.edu>
2016-09-16 01:40:19 -0500

    blah

--- python/day_company_booth_entry.py
+++ python/day_company_booth_entry.py
@@ -1,19 +1,27 @@
 #! /usr/bin/env python3
 
+# company_ids_day[index] holds the id of the company at booth[index] on that day
+
+# days are zero-indexed in code, but the db starts them at 1
 MONDAY_ID = 1
+FIRST_WEDNESDAY_ID = 2
+SECOND_WEDNESDAY_ID = 3
+
+TOTAL_NUM_TABLES = 117
+
 SQL_INSERT_COMPANY = '''
 INSERT INTO day_company_booth (
-    id,
     day_id,
     company_id,
     booth_id)
 VALUES (
-    %i, %i, %i, %i
+    %i, %i, %i
 );
 '''
 
-# company_ids[index] holds the id of the company at booth[index] on monday
-company_ids = [
+
+# ------------------------ BEGIN TABLE ASSIGNMENTS FOR DAY 1 (Monday):
+company_ids_1 = [
 None,
 None,
 None,
@@ -154,15 +162,308 @@ None,
 268
 ]
 
-for i in range(1, len(company_ids)):
 
-    company_id = company_ids[i]
+# ------------------------ BEGIN TABLE ASSIGNMENTS FOR DAY 2 (First Wednesday):
+company_ids_2 = [
+None,       #(0)
+None,
+None,
+None,
+None,
 
-    if company_id is None:
-        continue
+x,        #(5)
+x,
+x,
+x,
+x,
+x,
 
-    # Booth_id starts at zero and increments with x. For future maps, x 
-    # will start where this iteration ends, and booth_id will start over
-    # at zero and continue incrementing with x.
-    print (SQL_INSERT_COMPANY % (i, MONDAY_ID, company_id, i) )
+x,        #(11)
+x,
+x,
+x,
 
+x,        #(15)
+x,
+x,
+x,
+
+x,        #(19)
+x,
+x,
+x,
+
+x,        #(23)
+x,
+x,
+x,
+
+x,        #(27)
+x,
+x,
+x,
+
+x,        #(31)
+x,
+x,
+x,
+
+x,        #(35)
+x,
+x,
+x,
+
+x,        #(39)
+x,
+x,
+x,
+
+x,        #(43)
+x,
+x,
+x,
+
+x,        #(47)
+x,
+x,
+x,
+x,
+x,
+
+x,        #(53)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,          #(61)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,          #(70)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,           #(79)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,           #(87)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,            #(99)
+
+x,           #(100)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,            #(108)
+x,
+x,
+x,
+x,
+
+x,            #(113)
+x,
+x,
+x,
+x
+]
+
+
+# ------------------------ BEGIN TABLE ASSIGNMENTS FOR DAY 3 (second Wednesday):
+company_ids_3 = [
+None,       #(0)
+None,
+None,
+None,
+None,
+
+x,        #(5)
+x,
+x,
+x,
+x,
+x,
+
+x,        #(11)
+x,
+x,
+x,
+
+x,        #(15)
+x,
+x,
+x,
+
+x,        #(19)
+x,
+x,
+x,
+
+x,        #(23)
+x,
+x,
+x,
+
+x,        #(27)
+x,
+x,
+x,
+
+x,        #(31)
+x,
+x,
+x,
+
+x,        #(35)
+x,
+x,
+x,
+
+x,        #(39)
+x,
+x,
+x,
+
+x,        #(43)
+x,
+x,
+x,
+
+x,        #(47)
+x,
+x,
+x,
+x,
+x,
+
+x,        #(53)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,          #(61)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,          #(70)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,           #(79)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,           #(87)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,            #(99)
+
+x,           #(100)
+x,
+x,
+x,
+x,
+x,
+x,
+x,
+
+x,            #(108)
+x,
+x,
+x,
+x,
+
+x,            #(113)
+x,
+x,
+x,
+x
+]
+
+
+# INSERT day assignments
+for i in range(0, TOTAL_NUM_TABLES+1):
+    booth_id = i
+    company_id1 = company_ids_1[i]
+    company_id2 = company_ids_2[i]
+    company_id3 = company_ids_3[i]
+
+    if company_id1 is not None:
+        print (SQL_INSERT_COMPANY % (MONDAY_ID, company_id1, booth_id) )
+        
+    if company_id2 is not None:
+        print (SQL_INSERT_COMPANY % (FIRST_WEDNESDAY_ID, company_id2, booth_id) )
+    
+    if company_id3 is not None:
+        print (SQL_INSERT_COMPANY % (SECOND_WEDNESDAY_ID, company_id3, booth_id) )
+
+#reset i because I've ran this script like 2139586490 times
+i = 0
