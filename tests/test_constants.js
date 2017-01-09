40044628ff2d9e7cfbc792674037d82d0258bf14
Austin Hartzheim <inbox@austinhartzheim.me>
2017-01-09 07:04:35 +0000

    Removed excess logging.

--- tests/test_constants.js
+++ tests/test_constants.js
@@ -1,7 +1,6 @@
 describe('MAJOR_INDEXES', () => {
     it('should have a value for every value in MAJORS', () => {
         MAJORS.forEach((v) => {
-            console.log(v);
             expect(MAJOR_INDEXES[v]).toBeGreaterThan(-1);
         });
     });
