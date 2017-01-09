c1faa2b39a677f74ee14fe973ed10a1e3463d7f5
Austin Hartzheim <inbox@austinhartzheim.me>
2017-01-09 05:13:45 +0000

    Fix test to match current namespace.

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -1,5 +1,5 @@
 describe('utils.get_day_string()', () => {
     it('should return "No Map Data" if day index out of bounds', () => {
-        expect(utils.get_day_string(100)).toBe("No Map Data");
+        expect(get_day_string(100)).toBe("No Map Data");
     });
 });
